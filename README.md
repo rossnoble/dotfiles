a75790cae737f5e38b86f7eb473c96e703641e9a
Ross Noble <rosshnoble@gmail.com>
2017-10-11 11:31:46 -0700

    Add readme and install script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install
+
+```
+$ git clone git@github.com:rossnoble/dotfiles.git ~/.dotfiles
+$ cd .dotfiles
+$ ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,26 @@
+# Install home brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Dev tools
+brew install git
+brew install tmux
+brew install z
+brew install the_silver_searcher
+
+# # Ruby stuff
+brew install chruby
+brew install ruby-install
+brew install --HEAD https://raw.github.com/postmodern/gem_home/master/homebrew/gem_home.rb
+
+# Create symlinks for dotfiles
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/agignore ~/.agignore
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+
+# Other stuff
+brew cask install macdown
